87304f9fe48dfc35483b78aa6c090a37bc63f42f
fine <92whdtjs@gmail.com>
2024-11-02 16:51:45 +0900

    feat: #18 - 화면 전환 종류 정의

---
+++ Domado/Domado/Route/NavigationType/FullScreenDestination.swift
@@ -0,0 +1,14 @@
+//
+//  FullScreenDestination.swift
+//  Domado
+//
+//  Created by 이종선 on 11/2/24.
+//
+
+import Foundation
+
+/// FullScreen 형태로 보여줄 화면 목록입니다. 
+enum FullScreenDestination: Hashable{
+    case login
+    case onboarding
+}
---
+++ Domado/Domado/Route/NavigationType/SheetDestination.swift
@@ -0,0 +1,14 @@
+//
+//  SheetDestination.swift
+//  Domado
+//
+//  Created by 이종선 on 11/2/24.
+//
+
+import Foundation
+
+/// Sheet 형태로 화면상에 보여줄 화면 목록입니다. 
+enum SheetDestination: Hashable {
+    case qrScanner
+    
+}
