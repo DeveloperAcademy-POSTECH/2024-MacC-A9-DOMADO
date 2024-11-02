0392e102bdc083fd890408ca9e109c40b9ccef3b
fine <92whdtjs@gmail.com>
2024-11-02 21:10:23 +0900

    feat/#18 - RootView 구성
    AppRouter를 이용해 앱 전체 화면 플로우 관리 지점

--- Domado/Domado/View/RootView.swift
+++ Domado/Domado/View/RootView.swift
@@ -10,18 +10,74 @@ import SwiftUI
 struct RootView: View {
     
     @StateObject private var router = AppRouter()
+    @EnvironmentObject private var appState : AppState
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack(path: $router.path) {
+            Group {
+                switch appState.authState {
+                case .unknown:
+                    LoadingView()
+                case .authenticated:
+                    HomeView()
+                case .unauthenticated:
+                    LoginView()
+                }
+            }
+            .navigationDestination(for: NavigationDestination.self) { destination in
+                destinationView(for: destination)
+            }
+            .sheet(item: $router.activeSheet) { sheet in
+                
+            }
+            .fullScreenCover(item: $router.activeFullScreen) { fullScreen in
+                
+            }
+            
         }
-        .padding()
+        .environmentObject(router)
+    }
+    
+    // MARK: - View Builders
+    @ViewBuilder
+    private func destinationView(for destination: NavigationDestination) -> some View {
+        switch destination {
+        case .stationDetail:
+            StationDetailView()
+        case .inUse:
+            InUseBikeView()
+        case .tempLock:
+            TempLockView()
+        case .returnComplete:
+            ReturnBikeView()
+        }
+    }
+    
+    @ViewBuilder
+    private func sheetView(for sheet: SheetDestination) -> some View {
+        switch sheet {
+            case .qrScanner:
+                QRScannerView()
+        }
+    }
+    
+    @ViewBuilder
+    private func fullScreenView(for fullScreen: FullScreenDestination) -> some View {
+        switch fullScreen {
+        case .login:
+            LoginView()
+        case .onboarding:
+            OnboardingView()
+        }
     }
 }
 
-#Preview {
-    RootView()
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        // Mock AppState with sample data
+        let appState = AppState()
+        
+        return RootView()
+            .environmentObject(appState)
+    }
 }
