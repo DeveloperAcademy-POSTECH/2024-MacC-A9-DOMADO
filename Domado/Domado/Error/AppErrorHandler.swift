2d068799461bcbb46604e75ea225be4f2c46ae5a
fine <92whdtjs@gmail.com>
2024-11-10 13:17:31 +0900

    enh:#49 - AppGlobalErrorHandler @ObservableObject 제거

--- Domado/Domado/Error/AppErrorHandler.swift
+++ Domado/Domado/Error/AppErrorHandler.swift
@@ -5,6 +5,7 @@
 //  Created by 이종선 on 11/9/24.
 //
 
+/// AppGlobalErrorHandler를 wrapping하여 싱글톤 형태로 앱 전역에서 유저에게 쉽게 에러를 표시할 수 있습니다. 
 final class AppErrorHandler {
     static let shared = AppErrorHandler()
     private var errorHandler: AppGlobalErrorHandler?
