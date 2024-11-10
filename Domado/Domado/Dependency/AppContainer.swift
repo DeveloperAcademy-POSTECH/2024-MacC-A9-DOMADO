e927a99522cb516374eb3ff3e45c05b707e2e3dd
fine <92whdtjs@gmail.com>
2024-11-10 13:20:17 +0900

    enh: #49 - 생성자 내부에서 초기화 순서 명시적으로 관리

--- Domado/Domado/Dependency/AppContainer.swift
+++ Domado/Domado/Dependency/AppContainer.swift
@@ -15,38 +15,40 @@ class AppContainer {
     struct RootDependencies {
         let router: AppRouter
         let globalErrorState: GlobalErrorState
-        let globalErrorHandler: AppGlobalErrorHandler
     }
+    
+    /// 생성자 내부에서 초기화 순서 명시적 정의
+    init(){
+        // 1.먼저 에러 처리 시스템 초기화
+        self.gloablErrorState = GlobalErrorState()
+        self.globalErrorHandler = AppGlobalErrorHandler(errorState: gloablErrorState)
+        AppErrorHandler.shared.initialize(errorHandler: globalErrorHandler)
+        
+        // 2.에러 처리가 필요한 다른 컴포넌트 초기화
+        do {
+            self.stateStorage = try StateStorage()
+        } catch {
+            AppErrorHandler.shared.handleError(error)
+            self.stateStorage = StateStorage.createFallback()
+        }
+    }
+    
     
     // MARK: - 의존성 목록
     
-    /// 앱 상태 관리
-    private lazy var appState: AppState = {
-        AppState(storage: stateStorage)
-    }()
+    /// 1. 가장 먼저 초기화되어야 하는 에러 처리 관련 의존성
+    private let gloablErrorState: GlobalErrorState
+    private let globalErrorHandler: AppGlobalErrorHandler
     
-    /// 상태 저장소
-     private lazy var stateStorage: StateStorage = {
-         do {
-             return try StateStorage()
-         } catch {
-             //TODO: 에러 핸들링 적용
-             fatalError("Failed to initialize StateStorage: \(error)")
-         }
-     }()
+    /// 2. 에러 처리에 의존성에 의존하는 다른 컴포넌트들
+    // 앱 상태 저장소
+    private let stateStorage: StateStorage
+    // 앱 상태 관리
+    private lazy var appState: AppState = { AppState(storage: stateStorage) }()
     
     /// 화면 계층 관리
     private lazy var router: AppRouter = {
         AppRouter()
-    }()
-    
-    /// 표시될 에러 관리
-    private lazy var globalErrorState: GlobalErrorState = {
-        GlobalErrorState()
-    }()
-    
-    private lazy var globalErrorHandler: AppGlobalErrorHandler = {
-        AppGlobalErrorHandler(errorState: globalErrorState)
     }()
     
     /// 로깅 서비스
@@ -63,7 +65,7 @@ class AppContainer {
     
     /// RootView 의존성 주입
     func makeRootDependencies() -> RootDependencies {
-        RootDependencies(router: router, globalErrorState: globalErrorState, globalErrorHandler: globalErrorHandler)
+        RootDependencies(router: router, globalErrorState: gloablErrorState)
     }
     
 }
