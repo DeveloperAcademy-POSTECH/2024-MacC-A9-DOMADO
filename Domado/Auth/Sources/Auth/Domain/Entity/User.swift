7332f3f0d3357f7513ded3f3502a04ad39dc8180
fine <92whdtjs@gmail.com>
2024-11-20 22:00:17 +0900

    feat: #63 - User 엔티티 수정

--- Domado/Auth/Sources/Auth/Domain/Entity/User.swift
+++ Domado/Auth/Sources/Auth/Domain/Entity/User.swift
@@ -1,18 +1,21 @@
 //
-//  File.swift
+//  User.swift
 //  Auth
 //
 //  Created by 이종선 on 11/19/24.
 //
 
-public struct User: Codable {
-    let id: String
+public struct User: Codable, Sendable{
+    let id: Int
     let email: String
     let name: String
-    
-    public init(id: String, email: String, name: String) {
-        self.id = id
-        self.email = email
-        self.name = name
-    }
+    let phone: String
+    let status: String
+    let roles: [String]
+    let hasRegisteredPayments: Bool
+    let currentRentalId: Int?
+    let stampCount: Int
+    let couponCount: Int
+    let createdAt: String
+    let updatedAt: String
 }
