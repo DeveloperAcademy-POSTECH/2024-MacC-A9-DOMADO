b44b0218efa26414b59f82d9fc1f0896ee57b11f
kojaebo <150410280+kojaebo@users.noreply.github.com>
2024-11-21 01:08:08 +0900

    feat:#64-Add design system color assets

--- Domado/Core/Sources/Core/Extensions/Color+Extension.swift
+++ Domado/Core/Sources/Core/Extensions/Color+Extension.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 public extension Color {
-    static let darkBlue = Color("DarkBlue", bundle: .module)
-    static let deepBlue = Color("DeepBlue", bundle: .module)
-    static let lightBlue = Color("LightBlue", bundle: .module)
-
+ 
+    // Push Button
+       static let pushButtonDark = Color("pushButtonDark", bundle: .module)
+       static let pushButtonLight = Color("pushButtonLight", bundle: .module)
+       
+       // Interactive & Station
+       static let interactivePrimary = Color("interactivePrimary", bundle: .module)
+       static let stationDefault = Color("stationDefault", bundle: .module)
+       
+       // Hibike Colors
+       static let hibikeOrange = Color("hibikeOrange", bundle: .module)
+       static let hibikeYellowLight = Color("hibikeYellowLight", bundle: .module)
+       static let hibikeYellowBack = Color("hibikeYellowBack", bundle: .module)
+       static let hibikeYellowDefault = Color("hibikeYellowDefault", bundle: .module)
+       
+       // Location
+       static let locationMarker = Color("locationMarker", bundle: .module)
+       
+       // Gray Scale
+       static let grayScaleWhite = Color("grayScaleWhite", bundle: .module)
+       static let grayScaleOpacity60 = Color("grayScaleOpacity60", bundle: .module)
+       static let grayScaleLight = Color("grayScaleLight", bundle: .module)
+       static let grayScaleLightHover = Color("grayScaleLightHover", bundle: .module)
+       static let grayScaleLightActive = Color("grayScaleLightActive", bundle: .module)
+       static let grayScaleNormal = Color("grayScaleNormal", bundle: .module)
+       static let grayScaleDark = Color("grayScaleDark", bundle: .module)
+       static let grayScaleDarkHover = Color("grayScaleDarkHover", bundle: .module)
+       static let grayScaleDarker = Color("grayScaleDarker", bundle: .module)
+       
+       // Brown
+       static let brownLightActive = Color("brownLightActive", bundle: .module)
+       static let brownNormal = Color("brownNormal", bundle: .module)
 }
