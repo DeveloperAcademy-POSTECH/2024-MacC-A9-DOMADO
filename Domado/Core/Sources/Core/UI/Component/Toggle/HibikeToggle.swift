68b352bf58ac6b2d731afb24b0d332b38e9b0789
kojaebo <150410280+kojaebo@users.noreply.github.com>
2024-11-20 23:37:14 +0900

    feat:#64-토글 구현

---
+++ Domado/Core/Sources/Core/UI/Component/Toggle/HibikeToggle.swift
@@ -0,0 +1,67 @@
+//
+//  Toggle.swift
+//  Core
+//
+//  Created by 고재보 on 11/20/24.
+//
+
+import SwiftUI
+
+struct HibikeToggle: View {
+    @Binding var isOn: Bool
+    let action: () -> Void
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .fill(isOn ? .yellow : .white)
+                .frame(width: 358, height: 75)
+
+            HStack(spacing: 60) {
+                HStack(spacing: 3) {
+                    Text("하이바이크로 전환하기")
+                        .font(.system(size: 18))
+                        .fontWeight(.bold)
+                    
+                    helpButton
+                }
+                
+                toggleButton
+            }
+        }
+    }
+    
+    private var helpButton: some View {
+        Button {
+            // Sheet navigation logic here
+        } label: {
+            Image(systemName: "questionmark.circle")
+                .fontWeight(.bold)
+                .foregroundColor(.deepBlue)
+        }
+    }
+    
+    private var toggleButton: some View {
+        Toggle("", isOn: $isOn)
+            .labelsHidden()
+            .tint(.deepBlue)
+            .onChange(of: isOn) { oldValue, newValue in
+                action()
+            }
+    }
+}
+
+#Preview {
+    VStack {
+        HibikeToggle(isOn: .constant(false)) {
+            print("토글 상태가 변경되었습니다")
+        }
+        .padding()
+        
+        HibikeToggle(isOn: .constant(true)) {
+            print("토글 상태가 변경되었습니다")
+        }
+        .padding()
+    }
+    .background(Color.gray.opacity(0.5))
+}
