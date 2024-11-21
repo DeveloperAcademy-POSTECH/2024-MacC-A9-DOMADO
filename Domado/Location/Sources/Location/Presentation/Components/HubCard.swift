3889d8a7c98499965289209e6617c648e5fa5a14
yoomin <leeymin130@gmail.com>
2024-11-22 07:47:57 +0900

    feat: #71 - HubCard 컴포넌트 추가

---
+++ Domado/Location/Sources/Location/Presentation/Components/HubCard.swift
@@ -0,0 +1,63 @@
+//
+//  HubCard.swift
+//  Location
+//
+//  Created by yoomin on 11/22/24.
+//
+
+import SwiftUI
+import Core
+
+struct HubCard: View {
+    var width: CGFloat = 356
+    var height: CGFloat = 288
+    var hubName: String = "박태준 학술 정보관"
+    
+    var body: some View {
+        Rectangle()
+            .fill(Color.grayScaleWhite)
+            .overlay(
+                VStack(spacing: 0) {
+                    HStack(spacing: 8){
+                        Image(systemName: "mappin.circle.fill") // 이미지 대치
+                            .resizable()
+                            .frame(width: 22, height: 22)
+                            .foregroundStyle(Color.stationDefault)
+                        Text(hubName)
+                            .customFont(.station_lg_bold)
+                            .foregroundStyle(Color.grayScaleDarker)
+                    }
+                    .padding(.bottom, 14)
+                    
+                    Text("대여가능한 바이크 수")
+                        .customFont(.caption_md_semibold)
+                        .foregroundStyle(Color.grayScaleLightActive)
+                        .padding(.bottom, 5)
+                    
+                    HStack(spacing: 6){
+                        Image(systemName: "bicycle") // 이미지 대치
+                            .resizable()
+                            .frame(width: 22, height: 22)
+                            .foregroundStyle(Color.grayScaleLightActive)
+                        Text("5대")
+                            .customFont(.station_lg_bold)
+                            .foregroundStyle(Color.grayScaleDarker)
+                    }
+                    .padding(.bottom, 14)
+                    
+                    Spacer()
+                    
+                    StationView()
+                }
+                    .padding(18)
+            )
+            .clipShape(RoundedRectangle(cornerRadius: 25))
+            .frame(width: width, height: height)
+            .shadow(color: .black.opacity(0.25), radius: 4, x: 0, y: 0)
+        
+    }
+}
+
+#Preview {
+    HubCard()
+}
---
+++ Domado/Location/Sources/Location/Presentation/Components/StationView.swift
@@ -0,0 +1,113 @@
+//
+//  StationView.swift
+//  Location
+//
+//  Created by yoomin on 11/22/24.
+//
+
+import SwiftUI
+import Core
+
+struct StationView: View {
+    var width: CGFloat = 326
+    var height: CGFloat = 86
+    var stationName: String = "A 스테이션"
+    
+    var body: some View {
+        VStack(spacing: 0){
+            Text("바이크 배터리 상태")
+                .customFont(.caption_md_semibold)
+                .foregroundStyle(Color.grayScaleLightActive)
+                .padding(5)
+            
+            HStack(spacing: 0){
+                Button {
+                    
+                } label: {
+                    Image(systemName: "arrow.left")
+                        .bold()
+                }
+                
+                Text(stationName)
+                    .customFont(.button_md_semibold)
+                    .foregroundStyle(Color.grayScaleNormal)
+                    .padding(.horizontal, 73)
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "arrow.right")
+                        .bold()
+                }
+                
+            }
+            .padding(.bottom, 11)
+            
+            Rectangle()
+                .fill(Color.black)
+                .overlay(
+                    HStack {
+                        //ForEach
+                        Dock(number: 1, batteryLevel: nil)
+                        Dock(number: 2, batteryLevel: 56)
+                        Dock(number: 3, batteryLevel: nil)
+                        Dock(number: 4, batteryLevel: 78)
+                    }
+                )
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .frame(width: width, height: height)
+            
+        }
+    }
+}
+struct Dock: View {
+    let number: Int
+    let batteryLevel: Int?
+    
+    var body: some View {
+        VStack(spacing: 2) {
+            Text("\(number)")
+                .customFont(.caption_md_semibold)
+                .foregroundStyle(Color.pushButtonDark)
+            
+            if let batteryLevel = batteryLevel {
+                ZStack {
+                    Circle()
+                        .fill(Color.pushButtonDark)
+                        .frame(width: 54, height: 54)
+                    
+                    GeometryReader { geometry in
+                        Rectangle()
+                            .fill(Color.green)
+                            .frame(
+                                height: geometry.size.height * CGFloat(batteryLevel) / 100
+                            )
+                            .frame(
+                                maxHeight: .infinity,
+                                alignment: .bottom
+                            )
+                    }
+                    .clipShape(Circle())
+                    .frame(width: 54, height: 54)
+                    
+                    Text("\(batteryLevel)")
+                        .customFont(.batterynumber)
+                        .foregroundStyle(Color.grayScaleWhite)
+                }
+            } else {
+                Circle()
+                    .fill(Color.pushButtonLight)
+                    .frame(width: 54, height: 54)
+                    .overlay(
+                        Text("대여중")
+                            .customFont(.body_sm_regular)
+                            .foregroundStyle(Color.pushButtonDark)
+                    )
+            }
+        }
+    }
+}
+
+#Preview {
+    StationView()
+}
