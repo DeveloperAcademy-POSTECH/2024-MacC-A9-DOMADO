963ef0884c9bd9bf68b7da5f5bbf0b499a4540c3
fine <92whdtjs@gmail.com>
2024-11-21 12:40:31 +0900

    feat: #69 - AppUser : 유저 상태에 따른 앱 화면 전환 처리

---
+++ Domado/Core/Sources/Core/State/StateType/AppUser.swift
@@ -0,0 +1,24 @@
+//
+//  AppUser.swift
+//  Core
+//
+//  Created by 이종선 on 11/21/24.
+//
+
+public struct AppUser {
+    let id: Int
+    let name: String
+    let hasRegisteredPayments: Bool
+    let currentRentalId: Int?
+    let stampCount: Int
+    let couponCount: Int
+    
+    public init(id: Int, name: String, hasRegisteredPayments: Bool, currentRentalId: Int?, stampCount: Int, couponCount: Int) {
+        self.id = id
+        self.name = name
+        self.hasRegisteredPayments = hasRegisteredPayments
+        self.currentRentalId = currentRentalId
+        self.stampCount = stampCount
+        self.couponCount = couponCount
+    }
+}
