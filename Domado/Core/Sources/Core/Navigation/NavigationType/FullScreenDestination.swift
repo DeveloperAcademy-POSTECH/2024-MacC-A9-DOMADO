3abd0d20a5d97a0250a9d24017ef547f4226931f
fine <92whdtjs@gmail.com>
2024-11-22 02:24:05 +0900

    feat: #69 -  sheet case 추가

--- Domado/Core/Sources/Core/Navigation/NavigationType/FullScreenDestination.swift
+++ Domado/Core/Sources/Core/Navigation/NavigationType/FullScreenDestination.swift
@@ -9,11 +9,13 @@ import Foundation
 
 /// FullScreen 형태로 보여줄 화면 목록입니다. 
 public enum FullScreenDestination: Identifiable, Hashable{
+    case qrScanner
     case login
     case onboarding
     
     public var id: String {
         switch self {
+        case .qrScanner : return "qrScanner"
         case .login: return "login"
         case .onboarding: return "onboarding"
         }
