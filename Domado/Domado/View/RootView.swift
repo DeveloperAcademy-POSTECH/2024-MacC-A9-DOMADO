b4dc288798e0e0d700268f660be813580fcac7ef
fine <92whdtjs@gmail.com>
2024-11-22 12:26:01 +0900

    feat: #69 - RootView 화면 이동 로직 + View 별 세부 조정

--- Domado/Domado/View/RootView.swift
+++ Domado/Domado/View/RootView.swift
@@ -28,21 +28,32 @@ struct RootView: View {
                 switch appState.authState {
                 case .unknown:
                     LoadingView()
-                case .authenticated:
-                    HomeView()
-                case .unauthenticated:
-                    container.makeLoginView()
+                case .authenticated,.unauthenticated:
+                    container.makeHomeView()
                 }
             }
             .errorAlert(errorState: globalErrorState)
             .navigationDestination(for: NavigationDestination.self) { destination in
                 destinationView(for: destination)
+                    .navigationBarBackButtonHidden()
             }
-            .sheet(item: $router.activeSheet) { sheet in
-                sheetView(for: sheet)
-            }
+            // QR 스캐너 풀스크린일 때만 그 위에 시트를 보여줌
             .fullScreenCover(item: $router.activeFullScreen) { fullScreen in
-                fullScreenView(for: fullScreen)
+                switch fullScreen {
+                case .qrScanner:
+                    fullScreenView(for: fullScreen)
+                        .sheet(item: $router.activeSheet) { sheet in
+                            sheetView(for: sheet)
+                        }
+                default:
+                    fullScreenView(for: fullScreen)
+                }
+            }
+            // QR 스캐너가 아닐 때는 여기서 시트를 보여줌
+            .sheet(item: $router.activeSheet) { sheet in
+                if router.activeFullScreen != .qrScanner {
+                    sheetView(for: sheet)
+                }
             }
             
         }
@@ -52,12 +63,10 @@ struct RootView: View {
     @ViewBuilder
     private func destinationView(for destination: NavigationDestination) -> some View {
         switch destination {
-        case .stationDetail:
-            StationDetailView()
         case .inUse:
-            InUseBikeView()
+            container.makeInUserBikeView()
         case .tempLock:
-            TempLockView()
+            container.makeTempLockView()
         case .returnComplete:
             ReturnBikeView()
         case .singUp:
@@ -68,8 +77,19 @@ struct RootView: View {
     @ViewBuilder
     private func sheetView(for sheet: SheetDestination) -> some View {
         switch sheet {
-            case .qrScanner:
-            container.makeQRScannerView()
+        case .confirmRent:
+            container.makeRentConfirmView()
+                .presentationDetents([.medium])
+        case .confirmParking:
+            container.makeParkingConfirmView()
+                .presentationDetents([.medium])
+        case .showHiBikeGuide:
+            container.makeHiBikeGuideView()
+                .presentationDetents([.medium])
+        case .confirmUnParking:
+            container.makeUnparkingConfirmView()
+                .presentationDetents([.medium])
+            
         }
     }
     
@@ -80,6 +100,8 @@ struct RootView: View {
             container.makeLoginView()
         case .onboarding:
             container.makeOnboardingView()
+        case .qrScanner:
+            container.makeQRScannerView()
         }
     }
 }
