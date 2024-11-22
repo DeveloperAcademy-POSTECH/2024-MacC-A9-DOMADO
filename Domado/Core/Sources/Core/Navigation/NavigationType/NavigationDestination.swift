4d4e28b027271aecb46cb35516043146053bf6c9
fine <92whdtjs@gmail.com>
2024-11-22 12:24:56 +0900

    feat: #69 - 화면 전환 case 추가

--- Domado/Core/Sources/Core/Navigation/NavigationType/NavigationDestination.swift
+++ Domado/Core/Sources/Core/Navigation/NavigationType/NavigationDestination.swift
@@ -9,9 +9,6 @@ import Foundation
 
 /// NavigationStack 상에서 push, pop을 통해 이동가능한 화면 목록입니다.
 public enum NavigationDestination: Hashable {
-    /// 스테이션 상세 정보
-    //TODO: station을 연관값으로 받아 해당 station에 대한 DetailView를 보여줍니다.
-    case stationDetail
     /// 사용중
     case inUse
     /// 일시잠금
--- Domado/Core/Sources/Core/Navigation/NavigationType/SheetDestination.swift
+++ Domado/Core/Sources/Core/Navigation/NavigationType/SheetDestination.swift
@@ -10,10 +10,17 @@ import Foundation
 /// Sheet 형태로 화면상에 보여줄 화면 목록입니다. 
 public enum SheetDestination: Identifiable, Hashable {
     case confirmRent
+    case confirmParking
+    case showHiBikeGuide
+    case confirmUnParking
     
     public var id:String {
         switch self {
         case .confirmRent: return "confirmRent"
-    }
+        case .confirmParking: return "confirmParking"
+        case .showHiBikeGuide: return "showHiBikeGuide"
+        case .confirmUnParking: return "confirmUnParking"
+            
+        }
     }
 }
