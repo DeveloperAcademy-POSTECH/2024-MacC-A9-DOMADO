ae297a78523f181400eb6c4a4c2a4e6be9a32a59
fine <92whdtjs@gmail.com>
2024-11-25 01:15:59 +0900

    feat: #76 - 앱 시작시 push noti 권한 요청

--- Domado/Domado/App/DomadoApp.swift
+++ Domado/Domado/App/DomadoApp.swift
@@ -20,6 +20,9 @@ struct DomadoApp: App {
         self.dIContainer = dIContainer
         _appState = StateObject(wrappedValue: dIContainer.makeAppState())
         _globalErrorState = StateObject(wrappedValue: dIContainer.makeGlobalErrorState())
+        
+        // AppDelegate에 PushNotificationManager 설정
+        appDelegate.setPushNotificationManager(dIContainer.makePushNotificationManager())
     }
     
     var body: some Scene {
@@ -27,6 +30,11 @@ struct DomadoApp: App {
             RootView(container: dIContainer)
                 .environmentObject(appState)
                 .environmentObject(globalErrorState)
+                .task {
+                    // 앱 실행 시 권한 요청
+                    await dIContainer.makePushNotificationManager().requestAuthorization()
+                }
+            
         }
     }
 }
