4c774ac08d2cf1317a279ce786012eaf8266f522
fine <92whdtjs@gmail.com>
2024-11-27 12:26:55 +0900

    feat: #80 - HomeView 컴포넌트 추가

--- Domado/Domado/View/TempView/HomeView.swift
+++ Domado/Domado/View/TempView/HomeView.swift
@@ -6,55 +6,50 @@
 //
 
 import SwiftUI
+import Core
+import MapKit
 
 struct HomeView: View {
     
     @StateObject var vm: HomeViewModel
     
     var body: some View {
-        VStack {
-            // WebSocket 연결 상태 표시
-            HStack {
-                Circle()
-                    .fill(vm.isWebSocketConnected ? Color.green : Color.red)
-                    .frame(width: 10, height: 10)
+        ZStack{
+            Map(position: $vm.position) {
+                UserAnnotation()
+            }
+            .mapStyle(.standard)
+            .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                // WebSocket 연결 상태 표시
+                HStack {
+                    Circle()
+                        .fill(vm.isWebSocketConnected ? Color.green : Color.red)
+                        .frame(width: 10, height: 10)
+                    
+                    Text(vm.isWebSocketConnected ? "연결됨" : "연결 안됨")
+                        .font(.caption)
+                }
+                .padding()
                 
-                Text(vm.isWebSocketConnected ? "연결됨" : "연결 안됨")
-                    .font(.caption)
-            }
-            .padding()
-            
-            // 마지막 수신 메시지 표시 (있는 경우)
-            if let lastMessage = vm.lastMessage {
-                Text(lastMessage)
-                    .font(.subheadline)
-                    .padding()
-            }
-            
-            Spacer()
-            
-            HStack {
-                Button {
-                    // 내정보 처리
-                } label: {
-                    Text("내정보")
+                // 마지막 수신 메시지 표시 (있는 경우)
+                if let lastMessage = vm.lastMessage {
+                    Text(lastMessage)
+                        .font(.subheadline)
                         .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(8)
                 }
                 
-                Button {
+                Spacer()
+                
+                HomeInfoCard {
+                    print("내정보 쿠폰카드 보여주기")
+                } rentAction: {
                     vm.rentBikeWithQR()
-                } label: {
-                    Text("QR 찍고 자전거 대여하기")
-                        .padding()
-                        .background(Color.green)
-                        .foregroundColor(.white)
-                        .cornerRadius(8)
                 }
+                
+                
             }
-            .padding()
         }
         .alert("알림", isPresented: $vm.showAlert) {
             Button("확인", role: .cancel) { }
