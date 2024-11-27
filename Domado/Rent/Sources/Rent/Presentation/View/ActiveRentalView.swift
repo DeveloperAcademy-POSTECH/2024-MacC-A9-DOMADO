e7bc285db2b3370c23bb35b9de7991725ce57e35
fine <92whdtjs@gmail.com>
2024-11-27 17:48:49 +0900

    feat: #80 - ActiveRentView 구성

--- Domado/Rent/Sources/Rent/Presentation/View/ActiveRentalView.swift
+++ Domado/Rent/Sources/Rent/Presentation/View/ActiveRentalView.swift
@@ -6,13 +6,30 @@
 //
 
 import SwiftUI
+import Core
+import MapKit
 
-struct ActiveRentalView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct ActiveRentalView: View {
+    
+    @StateObject private var vm: ActiveRentViewModel
+    
+    public init(vm: ActiveRentViewModel) {
+        _vm = StateObject(wrappedValue: vm)
     }
-}
-
-#Preview {
-    ActiveRentalView()
+    
+    public var body: some View {
+        ZStack{
+            Map(position: $vm.position) {
+                UserAnnotation()
+            }
+            .mapStyle(.standard)
+            .overlay(alignment: .bottom) {
+                BikeInfoCard(remainingTime: vm.remainingTime, batteryLevel: vm.batteryLevel, isParked: vm.isParked) {
+                    // 자전거 주차하기
+                    vm.parkBike()
+                }
+                .padding()
+            }
+        }
+    }
 }
