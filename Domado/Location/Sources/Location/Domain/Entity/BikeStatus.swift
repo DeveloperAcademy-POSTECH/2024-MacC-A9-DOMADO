ae20bafabef6806fd5ee134887e2b6ffd7f7a253
fine <92whdtjs@gmail.com>
2024-11-29 23:30:56 +0900

    feat: #89 - 자전거 상태 enum 추가

---
+++ Domado/Location/Sources/Location/Domain/Entity/BikeStatus.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Location
+//
+//  Created by 이종선 on 11/29/24.
+//
+
+import Foundation
+
+public enum BikeStatus: String, Codable, Sendable {
+    case PARKED
+    case TEMPORARY_LOCKED
+}
+
