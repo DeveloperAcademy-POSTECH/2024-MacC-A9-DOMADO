bdc66b612109144015607a9008f2ff39dd836ed5
fine <92whdtjs@gmail.com>
2024-12-01 01:00:15 +0900

    feat: #97 - ActiveRentViewModel: AppState 결제 상태 구독

--- Domado/Rent/Sources/Rent/Presentation/ViewModel/ActiveRentViewModel.swift
+++ Domado/Rent/Sources/Rent/Presentation/ViewModel/ActiveRentViewModel.swift
@@ -5,6 +5,7 @@
 //  Created by 이종선 on 11/27/24.
 //
 
+import Combine
 import Core
 import Foundation
 import _MapKit_SwiftUI
@@ -14,11 +15,22 @@ public class ActiveRentViewModel: ObservableObject {
     @Published var remainingTime = "00:00"
     @Published var batteryLevel = "34km"
     @Published var isParked = false
+    @Published var isPaymentProcessing = false
     
     private let router: Routing
+    private let appState: AppState
     
-    public init(router: Routing){
-        self.router = router 
+    private var cancellables = Set<AnyCancellable>()
+    
+    public init(router: Routing, appState: AppState){
+        self.router = router
+        self.appState = appState
+        
+        appState.$isProcessingPayment
+            .sink { [weak self] isProcessing in
+                self?.isPaymentProcessing = isProcessing
+            }
+            .store(in: &cancellables)
     }
     
     func parkBike() {
