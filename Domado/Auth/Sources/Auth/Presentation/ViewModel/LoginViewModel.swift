03f0638e3238243135c8571f67665e08483d1f44
fine <92whdtjs@gmail.com>
2024-12-01 23:42:06 +0900

    feat: #101 - Login 완료시 HomeView로 화면 전환

--- Domado/Auth/Sources/Auth/Presentation/ViewModel/LoginViewModel.swift
+++ Domado/Auth/Sources/Auth/Presentation/ViewModel/LoginViewModel.swift
@@ -44,14 +44,12 @@ public final class LoginViewModel: ObservableObject {
                     password: password
                 )
                 
-                //MARK: currentRental 존재시 BikeStatus에 따라 AppState 업데이트
-//                if signInUser.currentRentalId != nil {
-//                    self.appState.updateRideState(.active)
-//                }
                 
                 let currentUser = AppUser(id: signInUser.id, name: signInUser.name, hasRegisteredPayments: signInUser.hasRegisteredPayments, currentRentalId: signInUser.currentRentalId, stampCount: signInUser.stampCount, couponCount: signInUser.couponCount)
                 
                 self.appState.updateUserState(to: currentUser)
+                
+                router.dismissFullScreen()
                 
             } catch let _ as AuthError {
                 //TODO: UseCase에서 로그인 정보 검증 로직 추가 + 검증에 따른 error 처리
