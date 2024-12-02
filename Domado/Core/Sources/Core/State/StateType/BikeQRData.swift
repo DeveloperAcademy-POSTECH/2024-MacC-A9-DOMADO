c0fc11043b8c6fe9c822169b2169fb5c42966125
fine <92whdtjs@gmail.com>
2024-12-02 10:05:42 +0900

    feat: #103 - QR 코드 데이터 구조 정의

---
+++ Domado/Core/Sources/Core/State/StateType/BikeQRData.swift
@@ -0,0 +1,23 @@
+//
+//  BikeQRData.swift
+//  Core
+//
+//  Created by 이종선 on 12/2/24.
+//
+
+import Foundation
+
+struct BikeQRData: Codable {
+    let bikeCode: String
+    let homeHub: HomeHub
+    
+    struct HomeHub: Codable {
+        let name: String
+        let location: Location
+        
+        struct Location: Codable {
+            let latitude: Double
+            let longitude: Double
+        }
+    }
+}
